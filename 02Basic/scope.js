2d4d18bb183a6210d5ce0367297c7b9d449dfdf8
Anairoz <zoriana.dmytryk@gmail.com>
2019-01-14 23:17:44 +0200

    king problem

--- 02Basic/scope.js
+++ 02Basic/scope.js
@@ -1,11 +1,29 @@
-// use var instead of let if we want make variable global outside the scopes
-let iAmGlobal = 'someValue'
-  console.log(iAmGlobal);
+// // use var instead of let if we want make variable global outside the scopes
+// let iAmGlobal = 'someValue'
+//   console.log(iAmGlobal);
+// if (true) {
+//    let iAmLocal = 'someMoreValue'
+//   iAmGlobal = 'superman'
+//   console.log(iAmGlobal);
+//   console.log(iAmLocal);
+// }
+// console.log(iAmLocal);
+// console.log(iAmGlobal);
+
+
+//King problem
+
+// let king = 'John'
+
 if (true) {
-   let iAmLocal = 'someMoreValue'
-  iAmGlobal = 'superman'
-  console.log(iAmGlobal);
-  console.log(iAmLocal);
+  // let king = 'Sam'
+
+  if (true) {
+    let king = 'Ram'
+    console.log(king);
+  }
 }
-console.log(iAmLocal);
-console.log(iAmGlobal);
+
+if (true) {
+  console.log("I am a second part " + king);
+}
