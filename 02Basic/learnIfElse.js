286853822a357e6253aa7d58efc261ae73f06765
Anairoz <zoriana.dmytryk@gmail.com>
2019-01-14 21:54:39 +0200

    github

---
+++ 02Basic/learnIfElse.js
@@ -0,0 +1,9 @@
+if (false) {
+  console.log('I am inside if Block');
+  console.log('I am still inside if Block ');
+} else if (false){
+  console.log('I am inside IF CAPS BLOCK ');
+
+}else{
+  console.log('NOTICE THIS PART');
+}
