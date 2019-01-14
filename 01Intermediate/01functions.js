697952789478708cec75041bbdae93f2b415e2e8
Anairoz <zoriana.dmytryk@gmail.com>
2019-01-14 23:52:13 +0200

    New level

---
+++ 01Intermediate/01functions.js
@@ -0,0 +1,19 @@
+let sayHello = function (name){
+  console.log(`Greeting message for ${name}`);
+  console.log(`Hey ${name}`);
+}
+
+// sayHello('John')
+
+let fullNameMaker = function(firstName, lastName){
+console.log('Welcome to LSO');
+console.log(`Happy to Have You, ${firstName} ${lastName}`);
+}
+
+fullNameMaker('John', 'Doe')
+
+let myAdder = function(num1, num2){
+  let added = num1+num2
+  return added
+}
+console.log(myAdder(4,7))
