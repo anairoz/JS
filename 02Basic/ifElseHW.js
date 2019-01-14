a88da1b341572d9a768d3a8da944b96ce393bca1
Anairoz <zoriana.dmytryk@gmail.com>
2019-01-14 22:16:16 +0200

    JS

---
+++ 02Basic/ifElseHW.js
@@ -0,0 +1,14 @@
+// Grade problem
+var studentMark = '0'
+
+if (studentMark==='10') {
+  console.log('Amazing');
+} else if (studentMark==='5') {
+  console.log('Good');
+} else if (studentMark==='3') {
+  console.log('poor');
+} else if (studentMark==='0') {
+  console.log('fail');
+} else {
+  console.log("ERROR");
+}
--- 02Basic/learnIfElse.js
+++ 02Basic/learnIfElse.js
@@ -1,9 +1,22 @@
-if (false) {
-  console.log('I am inside if Block');
-  console.log('I am still inside if Block ');
-} else if (false){
-  console.log('I am inside IF CAPS BLOCK ');
+// if (false) {
+//   console.log('I am inside if Block');
+//   console.log('I am still inside if Block ');
+// } else if (false){
+//   console.log('I am inside IF CAPS BLOCK ');
+//
+// }else{
+//   console.log('NOTICE THIS PART');
+// }
 
-}else{
-  console.log('NOTICE THIS PART');
+var whoIsHere = ''
+
+if (whoIsHere === 'user') {
+  console.log('Greeting message  for user');
+  console.log('Allow access to view all courses');
+} else if (whoIsHere === 'teacher') {
+  console.log('Greeting message for TEACHER');
+  console.log('Allow access to view all courses');
+} else {
+  console.log('Message: please verify your email');
+  console.log('Send user an email for verification');
 }
