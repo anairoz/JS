1d15139ebacf7cd78adfe5c6f1f0936a773be402
Anairoz <zoriana.dmytryk@gmail.com>
2019-01-14 23:33:03 +0200

    arrays

---
+++ 02Basic/marvels.js
@@ -0,0 +1,9 @@
+//datatype - Array
+
+const superHeroes = ['Iron Man', 'Spiderman', 'Hulck', 'Capt. America']
+console.log(superHeroes);
+console.log(superHeroes[0]);
+console.log(superHeroes[3]);
+console.log(superHeroes[superHeroes.length -3]);
+
+console.log(`We have ${superHeroes.length} super heroes`);
