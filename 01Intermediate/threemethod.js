ca8acd5f22897c5181234e2e0a4d42b48cb07fdf
Anairoz <zoriana.dmytryk@gmail.com>
2019-01-15 11:22:48 +0200

    arrays method

---
+++ 01Intermediate/threemethod.js
@@ -0,0 +1,21 @@
+const numbers = ['One', 'Two', 'Three', 'Four', 'Five', 'Six']
+
+
+// numberd[1]= 'SOMETHING'
+// console.log(numbers);
+
+//start
+
+// console.log(numbers.shift()) //returns lost element
+// console.log(numbers);
+// numbers.unshift('zero')
+// console.log(numbers);
+
+//end
+
+// console.log("The element to be deleted is: "numbers.pop())
+// console.log(numbers);
+numbers.push('Seven')
+console.log(numbers);
+
+//middle
