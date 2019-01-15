da25cf628d55d1c29954c8bb2092c2c708e70527
Anairoz <zoriana.dmytryk@gmail.com>
2019-01-15 19:05:20 +0200

    new level

---
+++ 02Intermediate/myObjects.js
@@ -0,0 +1,18 @@
+let myYoutubeVideo = {
+  title: 'Loops in JS',
+  videoLength: 15,
+  videoCreator: 'Hitesh',
+  videoDescription: 'this video video description'
+}
+
+// console.log(myYoutubeVideo);
+console.log(`Hey new Video on ${myYoutubeVideo.title} by ${myYoutubeVideo.videoCreator}`);
+ //JScourse
+
+ let jsCourse = {
+   title: 'Javascript course',
+   price: '$100',
+   description: 'full online course of Javascript'
+ }
+
+ console.log(`Hey new course "${jsCourse.title}" by ${myYoutubeVideo.videoCreator}, that costs ${jsCourse.price}. Description: ${jsCourse.description}`);
