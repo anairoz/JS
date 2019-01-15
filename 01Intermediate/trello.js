3556c4ecb0be61b4ac4278e4388860241448bc2c
Anairoz <zoriana.dmytryk@gmail.com>
2019-01-15 18:01:15 +0200

    updated

--- 01Intermediate/trello.js
+++ 01Intermediate/trello.js
@@ -1,9 +1,11 @@
 const days = ['Mon', 'Tue', 'Wed', 'Thr', 'Fri', 'Sat','Sun']
 
-console.log(days[0]);
+// console.log(days[0]);
+//
+// days.forEach(function (day, num){
+//   console.log(`starts with ${num+1} --${day}`);
+// })
 
-
-
-days.forEach(function (day, num){
-  console.log(`starts with ${num+1} --${day}`);
-})
+for (let i = days.length-1; i >=0; i--) {
+  console.log(days[i])
+}
