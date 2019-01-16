5b4884db3a1a0a5a0dc10da22766745cec8ce6fb
Anairoz <zoriana.dmytryk@gmail.com>
2019-01-16 19:16:12 +0200

    random

---
+++ 02Intermediate/dice.js
@@ -0,0 +1,17 @@
+let num1 = 22
+let num2 = 7
+
+let result = num1 / num2
+// console.log(result.toFixed(2));
+let floatRes = result.toFixed(2)
+
+// console.log(Math.floor(floatRes));
+// console.log(Math.ceil(floatRes));
+
+// console.log(Math.random() * 10   + 43);
+
+ let upper = 23
+ let lower = 21
+
+ let myDandom = Math.floor(Math.random() * (upper - lower + 1 )) + lower
+console.log(myDandom);
